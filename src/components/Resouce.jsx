991a3b35a43833790d081a184cd7f611fa241394
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2024-05-19 08:04:03 +0530

    add event positioming

--- src/components/Resouce.jsx
+++ src/components/Resouce.jsx
@@ -3,40 +3,79 @@ import React from 'react'
 const Resouce = ({ nofDays }) => {
     const resourceList = ['A', "B", "C", "D", "E", "F", "H", "I", "J", "K", "L", "M", "N", "O"]
 
+    const handleClick = (e) => {
+        e.stopPropagation()
+
+        var overflowElement = e.currentTarget.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+        var rect = overflowElement.getBoundingClientRect();
+        console.log(rect);
+        const innerDim = e.target.getBoundingClientRect()
+        console.log(innerDim);
+        // Calculate the mouse click position within the viewport
+        // var x = e.clientX - rect.left;
+        // var y = e.clientY - rect.top;
+        // console.log(x);
+
+        // Add the scroll position to the calculated click position
+        var scrollX = overflowElement.scrollLeft;
+        var scrollY = overflowElement.scrollTop;
+        console.log(scrollX);
+
+        // var clickX = x + scrollX;
+        // var clickY = y + scrollY;
+
+        // console.log('Click position relative to overflow element: ', clickX, clickY);
+
+        const eventElement = document.createElement('div')
+        eventElement.style.position = 'absolute';
+        eventElement.style.left = `${innerDim.x + scrollX - 170}px`
+        eventElement.classList.add("innerElement")
+        e.target.parentElement.previousElementSibling.appendChild(eventElement)
+        console.log(e.target);
+    }
     const totalDays = nofDays.map((monthDate) =>
-        <div className={`min-w-20 min-h-20 border-2 border-l-0 border-t-0  border-cyan-500  `} key={monthDate.date}>
+        <div className={`min-w-20 min-h-20 border-2 border-l-0 border-t-0  border-cyan-500  `} key={monthDate.date} onClick={handleClick}>
         </div>
     )
-    const handleClick = (e) => {
-        console.log(e.target)
-        const element = e.target
-        console.log(e.clientX);
+    const handleParentClick = (e) => {
+        console.log(e.target.getBoundingClientRect())
+        var rect = e.target.getBoundingClientRect();
+        // const element = e.target
+        console.log(rect.x);
+        var x = e.clientX - rect.left;
+        console.log(x);
+        // const left = rect.x
+        var scrollX = e.target.scrollLeft;
+        console.log(scrollX);
 
-        const diff = e.clientX
+        var clickX = (x + scrollX) / 80;
+        const leftNum = Math.floor(clickX)
+        console.log(Math.floor(clickX));
+
         const eventElement = document.createElement('div')
-        e.target.classList.add("parentElement")
+        // e.target.classList.add("parentElement")
         eventElement.style.position = 'absolute';
-        // eventElement.style.width = "90%"
-        // eventElement.style.backgroundColor = "grey"
+        // // eventElement.style.width = "90%"
+        // // eventElement.style.backgroundColor = "grey"
+        eventElement.style.left = `${leftNum * 80 + 4}px`
         eventElement.classList.add("innerElement")
-        const num = Math.floor(((diff) - 172) / 80)
-        console.log(num + 1);
+        // eventElement.classList.add(`left:${rect.x}`)
 
-        eventElement.style.left = `${num * 80 + 6}px`
+        // console.log(eventElement.style.left);
+
         // eventElement.style.bottom = "0px"
         // eventElement.classList.add(`left-[${diff}px]`)
-        // e.target.
-        e.target.parentElement.firstChild.appendChild(eventElement)
-        // console.log(e.target.parentElement.firstChild);
+        e.target.appendChild(eventElement)
+        // e.target.parentElement.previousElementSibling.appendChild(eventElement)
 
-    }
-    const handleParentClick = (e) => {
+        // e.target.parentElement.firstChild.appendChild(eventElement)
+        console.log(e.target);
 
     }
     const totalCol = resourceList.map((res) => {
         return (
             <div className='relative'>
-                <div className='absolute min-h-16 min-w-[90%]' onClick={handleParentClick}></div>
+                <div className='absolute min-h-16 min-w-full' onClick={handleParentClick}></div>
                 <div className='flex'>{totalDays}</div>
             </div>
         )
@@ -52,7 +91,7 @@ const Resouce = ({ nofDays }) => {
             <div className='flex flex-col sticky z-10 left-0 min-w-44 '>
                 {resourceList.map((list) => <div className='min-w-24 min-h-20 border-2 border-b-0 border-cyan-500'>Resource {list} </div>)}
             </div>
-            <div onClick={handleClick}>
+            <div >
 
                 {totalCol}
 
